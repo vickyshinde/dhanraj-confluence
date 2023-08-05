5bc734c6f035098b7fc3778a3ee2fba111c9dc5b
Vicky Shinde <vicky.shinde@gmail.com>
2023-08-05 11:06:54 +0530

    sort and search feature

--- src/components/ListingPage.jsx
+++ src/components/ListingPage.jsx
@@ -7,10 +7,18 @@ import {
   Text,
   Table,
   Button,
+  TextInput,
+  Select,
   useMantineTheme,
 } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { ExternalLink, Trash, Edit } from "tabler-icons-react";
+import {
+  ExternalLink,
+  Trash,
+  Edit,
+  ChevronDown,
+  ChevronUp,
+} from "tabler-icons-react";
 
 const ListingPage = () => {
   const theme = useMantineTheme();
@@ -76,10 +84,60 @@ const ListingPage = () => {
     });
   };
 
+  const [sortActive, setSortActive] = useState({
+    columnName: "",
+    sortBy: "",
+  });
+
+  useEffect(() => {
+    setSortActive({
+      columnName: controller.sortColumn,
+      sortBy: controller.order,
+    });
+  }, [controller]);
+  console.log(sortActive);
+  const sorting = (col) => {
+    setController({
+      ...controller,
+      sortColumn: col,
+      order: controller.order === "asc" ? "desc" : "asc",
+      currentPage: 1,
+    });
+  };
+
+  const searchItems = (searchValue) => {
+    setController({
+      ...controller,
+      searchInput: searchValue,
+      currentPage: 1,
+    });
+    // highlightText
+  };
+
+  const showActive = (active) => {
+    if (sortActive.columnName === active && sortActive.sortBy === "asc") {
+      return <ChevronUp size={14} />;
+    } else if (
+      sortActive.columnName === active &&
+      sortActive.sortBy === "desc"
+    ) {
+      return <ChevronDown size={14} />;
+    } else {
+      return (
+        <Box
+          sx={{ display: "flex", flexDirection: "column", marginLeft: "20px" }}
+        >
+          <ChevronUp size={14} />
+          <ChevronDown size={14} />
+        </Box>
+      );
+    }
+  };
+
   const rows = recordList.map((item, index) => (
     <tr key={item.id}>
       <td style={{ width: "8%" }}>DCT-{item.id.toString().padStart(2, "0")}</td>
-      <td style={{ width: "8%" }}>{item.product}</td>
+      <td style={{ width: "10%" }}>{item.product}</td>
       <td style={{ width: "12%" }}>{item.components}</td>
       <td style={{ width: "auto" }}>{item.category}</td>
       <td style={{ width: "10%" }}>{item.type}</td>
@@ -169,17 +227,118 @@ const ListingPage = () => {
 
   return (
     <Box component="div" maw={1200} mx="auto" pos="relative">
+      <Grid>
+        <Grid.Col span={2}>
+          <TextInput
+            label="Type"
+            placeholder="type"
+            withAsterisk
+            mt="md"
+            onChange={(e) => searchItems(e.target.value)}
+            value={controller.searchInput}
+          />
+        </Grid.Col>
+        {/* <Grid.Col span={10}>
+          <Grid>
+            <Grid.Col span={2}>
+              <Select
+                withAsterisk
+                label="Components"
+                placeholder="Select"
+                mt="md"
+                data={[
+                  { value: "Switch", label: "Switch" },
+                  { value: "TOMAS", label: "TOMAS" },
+                  { value: "BIG", label: "BIG" },
+                  { value: "Admin Portal", label: "Admin Portal" },
+                  { value: "Merchant Portal", label: "Merchant Portal" },
+                  { value: "CBDC App", label: "CBDC App" },
+                  { value: "NGINX", label: "NGINX" },
+                  { value: "Bundle", label: "Bundle" },
+                ]}
+              />
+            </Grid.Col>
+          </Grid>
+        </Grid.Col> */}
+      </Grid>
+
       <LoadingOverlay visible={visible} overlayBlur={2} />
       {apiError && apiError}
       <Table mt="xl" striped>
         <thead>
           <tr>
-            <th>No</th>
-            <th>Product</th>
-            <th>Components</th>
-            <th>Category</th>
-            <th>Type</th>
-            <th>Topic</th>
+            <th onClick={() => sorting("id")}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                }}
+              >
+                No1111{showActive("id")}
+              </Box>
+            </th>
+            <th onClick={() => sorting("product")}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                }}
+              >
+                Product{showActive("product")}
+              </Box>
+            </th>
+            <th onClick={() => sorting("components")}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                }}
+              >
+                Components{showActive("components")}
+              </Box>
+            </th>
+            <th onClick={() => sorting("category")}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                }}
+              >
+                Category{showActive("category")}
+              </Box>
+            </th>
+            <th onClick={() => sorting("type")}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                }}
+              >
+                Type{showActive("type")}
+              </Box>
+            </th>
+            <th onClick={() => sorting("topic")}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                }}
+              >
+                Topic{showActive("topic")}
+              </Box>
+            </th>
             <th>Description</th>
             <th>Action</th>
           </tr>
